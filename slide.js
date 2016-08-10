6656879271f4176cde820b81959016c140f7d963
liushangyu <liushangyu@lnm.imech.ac.cn>
2016-08-10 18:35:34 +0800

    finished

---
+++ slide.js
@@ -0,0 +1,163 @@
+	function slider(sliders){
+		this._init(sliders);
+		this._preNext();
+		this._initListener();
+		this._turnSlider();
+	}
+	
+	
+	slider.prototype = {
+		index : 0,
+		num : 0,
+		handleTurn : "",
+		_init : function(sliders){
+			this.num = sliders.length;
+			this.sliders = sliders;
+			this._renderSliders(this.sliders);
+			return this;
+		},
+		_initListener : function(){
+			var _self = this;
+			var template_ctrl = this._getDom("#template_ctrl").childNodes;
+			for(var i = 0; i < template_ctrl.length; i++){
+				
+				
+				(function(i){
+					template_ctrl[i].addEventListener("click",function(){
+						_self._switchSlider(i,"");
+					});
+					})(i);
+				
+				/*
+				template_ctrl[i].addEventListener("click",function(){
+					(function(){
+						var _i = i;
+						alert(_i);
+						_self._switchSlider(_i,"");
+						})();
+						
+						
+					});/**/
+				}
+			},
+		_renderSliders : function(sliders){
+			var main_i = this._getDom("#template_main").innerHTML.replace(/^\s*/,'').replace(/\s*$/,'');
+			var ctrl_i = this._getDom("#template_ctrl").innerHTML.replace(/^\s*/,'').replace(/\s*$/,'');
+			
+			var main_html = [];
+			var ctrl_html = [];
+			var length = sliders.length;
+			for(var i = 0; i< length; i++){
+				var _html_main = main_i.replace(/{{index}}/g,sliders[i].index)
+									   .replace(/{{src}}/g,sliders[i].src)
+									   .replace(/{{class}}/g,sliders[i].class)
+									   .replace(/{{h2}}/g,sliders[i].h2)
+									   .replace(/{{h3}}/g,sliders[i].h3)
+									   .replace(/{{href}}/g,sliders[i].href);
+				var _html_ctrl = ctrl_i.replace(/{{index}}/g,sliders[i].index)
+									   .replace(/{{src}}/g,sliders[i].src);
+				main_html.push(_html_main);
+				ctrl_html.push(_html_ctrl);
+				}
+			this._getDom("#template_main").innerHTML = main_html.join("");
+			this._getDom("#template_ctrl").innerHTML = ctrl_html.join("");
+		},
+		
+		
+		_switchSlider : function(index,mode){
+			var _self = this;
+			var newClass = "";
+			var oldClass = 'main-i_left|main-i_active|main-i_right|main-i_pre|main-i_next';
+			var dom = this._getDom("#template_main").childNodes;
+			if(mode == "next"){
+				/*main-i_active --> main-i_left*/
+				_self._changeClass("main-i_pre",oldClass,dom[_self.index]);
+				/*main-i_active的下一个 --> main-i_right --> main-i_active*/
+				_self.index = (_self.index + 1) % _self.num;
+				_self._changeClass("main-i_right",oldClass,dom[_self.index]);
+				setTimeout(_active,100);
+				function _active(){
+					_self._changeClass("main-i_active",oldClass,dom[_self.index]);
+				}
+			}else if(mode == "pre"){
+				/*main-i_active --> main-i_left*/
+				_self._changeClass("main-i_next",oldClass,dom[_self.index]);
+				/*main-i_active的下一个 --> main-i_right --> main-i_active*/
+				_self.index = (_self.index + _self.num - 1) % _self.num;
+				_self._changeClass("main-i_left",oldClass,dom[_self.index]);
+				setTimeout(_active,100);
+				function _active(){
+					_self._changeClass("main-i_active",oldClass,dom[_self.index]);
+				}
+				
+			}else{
+				
+				if(index > _self.index){
+					/*main-i_active --> main-i_pre*/
+					_self._changeClass("main-i_pre",oldClass,dom[_self.index]);
+					/*index在右 --> main-i_right --> main-i_active*/
+					_self.index = index;
+					_self._changeClass("main-i_right",oldClass,dom[_self.index]);
+					setTimeout(_active,100);
+					function _active(){
+						_self._changeClass("main-i_active",oldClass,dom[_self.index]);
+					}
+					
+				}else if(index < _self.index){
+					/*main-i_active --> main-i_next*/
+					_self._changeClass("main-i_next",oldClass,dom[_self.index]);
+					/*index在右 --> main-i_left --> main-i_active*/
+					_self.index = index;
+					_self._changeClass("main-i_left",oldClass,dom[_self.index]);
+					setTimeout(_active,100);
+					function _active(){
+						_self._changeClass("main-i_active",oldClass,dom[_self.index]);
+					}
+				}
+				
+			}
+			this.index = _self.index;
+		},
+		
+		_changeClass : function(newClass,oldClass,_dom){
+			newClass = _dom.getAttribute("class").replace(new RegExp(oldClass),newClass);
+			_dom.setAttribute("class",newClass);
+		},
+		
+		
+		_turnSlider : function(){
+			var _self = this;
+			var _interval = 3000;
+			function _turn(){
+				_self._switchSlider((_self.index+1)%_self.num,"next");
+				}
+			this._getDom(".slider")[0].onmouseenter = function(){
+				clearInterval(_self.handleTurn);
+				}
+			this._getDom(".slider")[0].onmouseleave = function(){
+				_self.handleTurn = setInterval(_turn,_interval);
+				}
+			_self.handleTurn = setInterval(_turn,_interval);
+			this.index = _self.index;
+			this.handleTurn = _self.handleTurn;
+		},
+		_preNext : function(){
+			var _self = this;
+			_self._getDom("#pre").onclick = function(){
+				_self._switchSlider((_self.index+4)%_self.num,"pre");
+				}
+			_self._getDom("#next").onclick = function(){
+				_self._switchSlider((_self.index+1)%_self.num,"next");
+				}
+			},
+		
+		_getDom : function(name){
+			if(name.substr(0,1) == "."){
+				return document.getElementsByClassName(name.substr(1));
+			}
+			else if(name.substr(0,1) == "#"){
+				return document.getElementById(name.substr(1));
+			}
+		}
+		
+	}	
